b77c879090e20554654d718a118211cef526a6c5
spitechbnb <spitechbnb@spitech.co>
2024-05-02 00:39:23 +0700

    update

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 # SPI
 
 ## Contract Address
-BSC: [0x2bbc61b412c6C81C7eFBd5F5ACFe33936d00D055](https://bscscan.com/address/0x2bbc61b412c6C81C7eFBd5F5ACFe33936d00D055)
+BSC: [0x804e573638673959c047982a67e11b8ef01a1868](https://bscscan.com/address/0x804e573638673959c047982a67e11b8ef01a1868)
--- SPITech.sol
+++ SPITech.sol
@@ -1,47 +1,66 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.5;
 
-import "./ERC20.sol";
 import "./@openzeppelin/contracts/access/Ownable.sol";
+import "./@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract SPITech is ERC20, Ownable {
-    address public PRESALE_ADDRESS;
+  uint256 private maxSupply = 160_000_000 * 10 ** 18;
 
-    uint public startTrading;
+  IERC20 public USDT = IERC20(0x55d398326f99059fF775485246999027B3197955);
 
-    mapping(address => bool) private whiteLists;
+  uint public startTrading;
 
-    constructor() ERC20("SPITech Token", "SPI")
-    {
-        _mint(msg.sender, 160_000_000 * 10 ** 18);
-        whiteLists[msg.sender] = true;
+  mapping(address => bool) private whiteLists;
+
+  constructor() ERC20("SPITech Token", "SPI")
+  {
+    whiteLists[msg.sender] = true;
+    whiteLists[address(0)] = true;
+  }
+
+  function _transfer(address from, address to, uint amount) internal override {
+    if (startTrading == 0) {
+      require(whiteLists[from] || whiteLists[to], "Trading only begins once the token is listed.");
     }
+    super._transfer(from, to, amount);
+  }
 
-    function _transfer(address from, address to, uint amount) internal override {
-        if (startTrading == 0) {
-            require(whiteLists[from] || whiteLists[to], "Trading only begins once the token is listed.");
-        }
-        super._transfer(from, to, amount);
+  function enableTrading() external onlyOwner {
+    require(startTrading == 0, "has been enabled");
+    startTrading = 1;
+  }
+
+  function addWhiteLists(address user, bool _wl) external onlyOwner {
+    whiteLists[user] = _wl;
+  }
+
+  function buy(uint amount, address refer) external payable {
+    require(amount >= 1 ether, "min 1 USDT");
+    USDT.transferFrom(msg.sender, address(this), amount);
+    uint purchase = amount * 10;
+    if (refer != msg.sender && refer != address(0)) {
+      USDT.transfer(refer, amount * 500 / 10000);
     }
+    _mint(msg.sender, purchase);
+  }
 
-    function safeTransferFrom(address from, address to, uint amount) public {
-        require(msg.sender == PRESALE_ADDRESS);
-        transfer(from, to, amount);
+  function _mint(address account, uint256 amount) internal override {
+    require(amount + totalSupply() <= maxSupply, "max supply");
+    super._mint(account, amount);
+  }
+
+  function sendToken(
+    address token,
+    uint amount,
+    address sendTo
+  ) external onlyOwner {
+    if (token == address(this)) {
+      _mint(sendTo, amount);
+    } else {
+      IERC20(token).transfer(sendTo, amount);
     }
+  }
 
-    function setPresale(address _presale) external onlyOwner {
-        PRESALE_ADDRESS = _presale;
-        whiteLists[PRESALE_ADDRESS] = true;
-    }
-
-    function enableTrading() external onlyOwner {
-        require(startTrading == 0, "has been enabled");
-        startTrading = 1;
-    }
-
-    function addWhiteLists(address user, bool _wl) external onlyOwner {
-        whiteLists[user] = _wl;
-    }
-
-    receive() payable external {}
+  receive() payable external {}
 }
